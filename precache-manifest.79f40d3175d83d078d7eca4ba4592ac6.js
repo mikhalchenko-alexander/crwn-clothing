40abb8524856bea83cdcc276e009ac3ef1461650
aam <aam@anahoret.com>
2020-05-15 17:15:35 +0300

    Updates

---
+++ precache-manifest.79f40d3175d83d078d7eca4ba4592ac6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fcc5804778ec3a146457ffdf3e60a01",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "5cba9cd9b38884556e78",
+    "url": "/crwn-clothing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "265507e54105a27dffe6",
+    "url": "/crwn-clothing/static/js/2.090f5227.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crwn-clothing/static/js/2.090f5227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cba9cd9b38884556e78",
+    "url": "/crwn-clothing/static/js/main.17690bd4.chunk.js"
+  },
+  {
+    "revision": "0e46efea361dabb92088",
+    "url": "/crwn-clothing/static/js/runtime-main.4e42e579.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crwn-clothing/static/media/logo.5d5d9eef.svg"
+  }
+]);
