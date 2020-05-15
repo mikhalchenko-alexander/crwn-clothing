120275a08bf253dad0e5c63f56bca193715cce3e
aam <aam@anahoret.com>
2020-05-15 18:33:38 +0300

    Move pages out of components folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import HomePage from './components/pages/HomePage/HomePage.component'
+import HomePage from './pages/HomePage/HomePage.component'
 
 function App() {
   return (
