a8c5eedd3060076775f93b7bb8c042fd90990490
aam <aam@anahoret.com>
2020-05-15 20:20:44 +0300

    Extract menu and menu item to components

--- src/pages/HomePage/HomePage.component.jsx
+++ src/pages/HomePage/HomePage.component.jsx
@@ -1,47 +1,11 @@
 import React from 'react';
-import './HomePage.styles.scss'
+import './HomePage.styles.scss';
+import DirectoryMenu from '../../components/DirectoryMenu/DirectoryMenu.component';
 
 const HomePage = () => (
   <div className="home-page">
-    <div className="directory-menu">
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">JACKETS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SNEAKERS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMENS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MENS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-    </div>
+    <DirectoryMenu />
   </div>
 );
 
-export default HomePage
+export default HomePage;
