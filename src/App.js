643c0eb90d21acc86ed0b8624667def9952715db
aam <aam@anahoret.com>
2020-05-15 18:06:42 +0300

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import HomePage from './components/pages/HomePage/HomePage.component'
 
 function App() {
   return (
     <div>
-      Crown clothing E-Commerce site
+      <HomePage />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Crown clothing E-commerce site/i);
-  expect(linkElement).toBeInTheDocument();
+  const result = render(<App />);
+  expect(result.container).toBeTruthy();
 });
