75f15a485f35f24a45a569996303fb3ac5511713
aam <aam@anahoret.com>
2020-05-15 17:34:00 +0300

    Cleanup default stuff

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Crown clothing E-commerce site/i);
   expect(linkElement).toBeInTheDocument();
 });
