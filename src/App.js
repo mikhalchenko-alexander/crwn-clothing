0116c4c8e584140afd91c668a61a528dfc4548d6
aam <aam@anahoret.com>
2020-05-16 14:58:36 +0300

    Add header to all pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.scss';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage.component';
 import ShopPage from './pages/ShopPage/ShopPage.component';
+import Header from './components/Header/Header.component';
 
 function App() {
   return (
     <div>
-      <Route exact={ true } path="/" component={ HomePage } />
-      <Route exact={ true } path="/shop" component={ ShopPage } />
+      <Header />
+      <Switch>
+        <Route exact={ true } path="/" component={ HomePage } />
+        <Route exact={ true } path="/shop" component={ ShopPage } />
+      </Switch>
     </div>
   );
 }
