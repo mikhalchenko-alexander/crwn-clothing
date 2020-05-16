90e2d718e7ce3bb94e9e22a9b10b7c59ae4cba4d
aam <aam@anahoret.com>
2020-05-16 13:16:25 +0300

    Add shop page route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.scss';
 import { Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage.component';
-import HatsPage from './pages/HatsPage/HatsPage.component';
+import ShopPage from './pages/ShopPage/ShopPage.component';
 
 function App() {
   return (
     <div>
       <Route exact={ true } path="/" component={ HomePage } />
-      <Route exact={ true } path="/shop/hats" component={ HatsPage } />
+      <Route exact={ true } path="/shop" component={ ShopPage } />
     </div>
   );
 }
