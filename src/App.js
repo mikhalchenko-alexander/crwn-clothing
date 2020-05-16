b11233cc936d8fd18fac437bac51ca00405ea6f2
aam <aam@anahoret.com>
2020-05-16 15:30:54 +0300

    Add sign in page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage.component';
 import ShopPage from './pages/ShopPage/ShopPage.component';
 import Header from './components/Header/Header.component';
+import SignInPage from './pages/SignInPage/SignInPage.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact={ true } path="/" component={ HomePage } />
         <Route exact={ true } path="/shop" component={ ShopPage } />
+        <Route exact={ true } path="/sign-in" component={ SignInPage } />
       </Switch>
     </div>
   );
---
+++ src/pages/SignInPage/SignInPage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SignInPage.styles.scss';
+import SignIn from '../../components/SignIn/SignIn.component';
+
+const SignInPage = () => (
+  <div className='sign-in-page'>
+    <SignIn />
+  </div>
+);
+
+export default SignInPage;
