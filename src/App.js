e74fa53aab97f1a608ca10080903b44817c34b5f
aam <aam@anahoret.com>
2020-05-18 17:38:54 +0300

    Listen to auth state changed event and remember current user

--- src/App.js
+++ src/App.js
@@ -5,18 +5,40 @@ import HomePage from './pages/HomePage/HomePage.component';
 import ShopPage from './pages/ShopPage/ShopPage.component';
 import Header from './components/Header/Header.component';
 import SignInPage from './pages/SignInPage/SignInPage.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact={ true } path="/" component={ HomePage } />
-        <Route exact={ true } path="/shop" component={ ShopPage } />
-        <Route exact={ true } path="/sign-in" component={ SignInPage } />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => this.setState({ currentUser: user }));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact={ true } path="/" component={ HomePage } />
+          <Route exact={ true } path="/shop" component={ ShopPage } />
+          <Route exact={ true } path="/sign-in" component={ SignInPage } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
