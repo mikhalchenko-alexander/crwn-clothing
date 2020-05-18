62aeaad95cec9dc7bb9b49386a09d18224bc168b
aam <aam@anahoret.com>
2020-05-18 19:15:43 +0300

    Fix undefined style issue when additionalClassNames are not passed

--- src/components/Button/Button.component.jsx
+++ src/components/Button/Button.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.styles.scss';
 
 const Button = ({ children, additionalClassNames, ...otherProps }) => (
-  <button className={`${additionalClassNames} button `} { ...otherProps }>
+  <button className={`${additionalClassNames || ''} button `} { ...otherProps }>
     { children }
   </button>
 );
