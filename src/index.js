4f43f0fa175cde97e9c76964bb589f78948ce098
aam <aam@anahoret.com>
2020-05-19 21:16:31 +0300

    Wrap application with Redux provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import { Provider as ReduxProvider} from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
+  <ReduxProvider>
+    <React.StrictMode>
+      <HashRouter basename={ process.env.PUBLIC_URL }>
+        <App />
+      </HashRouter>
+    </React.StrictMode>
+  </ReduxProvider>,
   document.getElementById('root')
 );
