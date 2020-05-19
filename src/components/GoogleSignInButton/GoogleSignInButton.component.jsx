c9398cbeeae86942c269d61e6ad5656ba9840038
aam <aam@anahoret.com>
2020-05-19 19:08:37 +0300

    Change Google sign in button type to button to not trigger submit event on log in form

--- src/components/GoogleSignInButton/GoogleSignInButton.component.jsx
+++ src/components/GoogleSignInButton/GoogleSignInButton.component.jsx
@@ -3,7 +3,7 @@ import Button from '../Button/Button.component';
 import './GoogleSignInButton.styles.scss';
 
 const GoogleSignInButton = (props) => (
-  <Button additionalClassNames='google-sign-in-button' { ...props }>SIGN IN WITH GOOGLE</Button>
+  <Button type="button" additionalClassNames='google-sign-in-button' { ...props }>SIGN IN WITH GOOGLE</Button>
 );
 
 export default GoogleSignInButton;
