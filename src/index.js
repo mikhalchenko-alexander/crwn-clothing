18eebd4c90bffdf1b72dc4c3eae4251f624335db
aam <aam@anahoret.com>
2020-05-20 15:44:20 +0300

    Pass Redux store to provider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
-import { Provider as ReduxProvider} from 'react-redux';
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.render(
-  <ReduxProvider>
+  <ReduxProvider store={ store }>
     <React.StrictMode>
       <HashRouter basename={ process.env.PUBLIC_URL }>
         <App />
