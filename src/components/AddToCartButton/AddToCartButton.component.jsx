7d090669d0aad87592c3e3cd421ee9e6c5d37ca3
aam <aam@anahoret.com>
2020-05-21 18:51:43 +0300

    Add add to cart button component

---
+++ src/components/AddToCartButton/AddToCartButton.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './AddToCartButton.styles.scss'
+import Button from '../Button/Button.component';
+
+const AddToCartButton = () => (
+  <Button additionalClassNames="add-to-cart-button">ADD TO CART</Button>
+);
+
+export default AddToCartButton;
