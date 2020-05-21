87250831f7b090362056685e1e16175adc60365d
aam <aam@anahoret.com>
2020-05-21 22:59:46 +0300

    Pass props to button component

--- src/components/AddToCartButton/AddToCartButton.component.jsx
+++ src/components/AddToCartButton/AddToCartButton.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './AddToCartButton.styles.scss'
+import './AddToCartButton.styles.scss';
 import Button from '../Button/Button.component';
 
-const AddToCartButton = () => (
-  <Button additionalClassNames="add-to-cart-button">ADD TO CART</Button>
+const AddToCartButton = (props) => (
+  <Button additionalClassNames="add-to-cart-button" { ...props }>ADD TO CART</Button>
 );
 
 export default AddToCartButton;
