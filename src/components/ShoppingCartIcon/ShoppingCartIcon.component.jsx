fd89403bbd17d1646db42c2893cb9ca18f8752ed
aam <aam@anahoret.com>
2020-05-21 18:18:42 +0300

    Connect shopping cart dropdown reducer to shopping cart icon component

--- src/components/ShoppingCartIcon/ShoppingCartIcon.component.jsx
+++ src/components/ShoppingCartIcon/ShoppingCartIcon.component.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './ShoppingCartIcon.styles.scss'
 import { ReactComponent as ShoppingBagIcon } from './shopping-bag.svg';
+import { createToggleHiddenShoppingCartDropdownAction } from '../../redux/shopping-cart-dropdown/shopping-cart-dropdown-actions';
+import { connect } from 'react-redux';
 
-const CartIcon = () => (
-  <div className="shopping-cart-icon">
+const CartIcon = ({ toggleCartHidden }) => (
+  <div onClick={ toggleCartHidden } className="shopping-cart-icon">
     <ShoppingBagIcon className="shopping-bag-icon" />
     <span className="item-count">0</span>
   </div>
 );
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(createToggleHiddenShoppingCartDropdownAction())
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);
