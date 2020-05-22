f26272a55c9b1b15a922d83dd1966b512746a29d
aam <aam@anahoret.com>
2020-05-22 15:56:34 +0300

    Show shopping cart items in shopping cart dropdown

--- src/components/ShoppingCartIcon/ShoppingCartDropdown/ShoppingCartDropdown.component.jsx
+++ src/components/ShoppingCartIcon/ShoppingCartDropdown/ShoppingCartDropdown.component.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './ShoppingCartDropdown.styles.scss';
 import Button from '../../Button/Button.component';
+import ShoppingCartItem from './ShoppingCartItem/ShoppingCartItem.component';
+import { connect } from 'react-redux';
 
-const ShoppingCartDropdown = () => (
+const ShoppingCartDropdown = ({ shoppingCartItems }) => (
   <div className="shopping-cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {
+        shoppingCartItems.map(item => <ShoppingCartItem key={ item.id } item={ item } />)
+      }
+    </div>
     <Button>GO TO CHECKOUT</Button>
   </div>
 );
 
-export default ShoppingCartDropdown;
+const mapStateToProps = state => ({
+  shoppingCartItems: state.shoppingCart.cartItems
+});
+
+export default connect(mapStateToProps)(ShoppingCartDropdown);
