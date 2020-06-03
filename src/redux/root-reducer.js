1467169d27319209f0bec051cf50e7acf4866794
aam <aam@anahoret.com>
2020-06-03 21:33:31 +0300

    Make redux store persistent

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,9 +2,19 @@ import { combineReducers } from 'redux';
 import userReducer from './user/user-reducer';
 import shoppingCartDropdownReducer from './shopping-cart/shopping-cart-dropdown/shopping-cart-dropdown-reducer';
 import shoppingCartReducer from './shopping-cart/shopping-cart-reducer';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['shoppingCart']
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   shoppingCartDropdown: shoppingCartDropdownReducer,
   shoppingCart: shoppingCartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistor = persistStore(store);
+
+export { store, persistor };
