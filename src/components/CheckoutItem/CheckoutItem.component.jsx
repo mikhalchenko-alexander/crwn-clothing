167e180c5ba1c699ad1c28c3f32b7d71b319eaf2
aam <aam@anahoret.com>
2020-06-03 17:31:41 +0300

    Clear item from cart on remove button click

--- src/components/CheckoutItem/CheckoutItem.component.jsx
+++ src/components/CheckoutItem/CheckoutItem.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './CheckoutItem.styles.scss';
+import { createClearItemFromShoppingCartAction } from '../../redux/shopping-cart/shopping-cart-actions';
+import { connect } from 'react-redux';
 
-const CheckoutItem = ({ item }) => (
+const CheckoutItem = ({ item, clearItemFromShoppingCart }) => (
   <div className="checkout-item">
     <div className="image-container">
       <img src={ item.imageUrl } alt="item" />
@@ -9,8 +11,12 @@ const CheckoutItem = ({ item }) => (
     <div className="name">{ item.name }</div>
     <div className="quantity">{ item.quantity }</div>
     <div className="price">${ item.price }</div>
-    <div className="remove-button">&#10005;</div>
+    <div className="remove-button" onClick={ () => clearItemFromShoppingCart(item.id) }>&#10005;</div>
   </div>
 );
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItemFromShoppingCart: itemId => dispatch(createClearItemFromShoppingCartAction(itemId))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
