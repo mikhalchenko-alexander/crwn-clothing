b1e3f50861ee577ebae1eededb0af8b24530079f
aam <aam@anahoret.com>
2020-06-05 17:42:15 +0300

    Show collection items on collection page

--- src/pages/CollectionPage/CollectionPage.component.jsx
+++ src/pages/CollectionPage/CollectionPage.component.jsx
@@ -3,12 +3,17 @@ import './CollectionPage.styles.scss';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { selectCollectionByName } from '../../redux/shop/shop-selectors';
+import CollectionItem from '../../components/CollectionPreview/CollectionItem/CollectionItem.component';
 
-const CollectionPage = ({ collection }) => (
-  <div className="collection-page">
-    <h1>{ collection.title }</h1>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return <div className="collection-page">
+    <h2 className="title">{ title }</h2>
+    <div className="items">
+      { items.map(item => <CollectionItem key={ item.id } item={ item } />) }
+    </div>
+  </div>;
+};
 
 const mapStateToProps = (state, ownProps) => createStructuredSelector({
   collection: () => selectCollectionByName(ownProps.match.params.collectionId)(state)
