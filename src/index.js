5800a0faa382e137e43feb8a5e0dcea5e8b8e780
aam <aam@anahoret.com>
2020-06-11 10:47:55 +0300

    Replace hash router with browser router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -10,11 +10,11 @@ import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.render(
   <ReduxProvider store={ store }>
     <React.StrictMode>
-      <HashRouter basename={ process.env.PUBLIC_URL }>
+      <BrowserRouter>
         <PersistGate persistor={ persistor }>
           <App />
         </PersistGate>
-      </HashRouter>
+      </BrowserRouter>
     </React.StrictMode>
   </ReduxProvider>,
   document.getElementById('root')
