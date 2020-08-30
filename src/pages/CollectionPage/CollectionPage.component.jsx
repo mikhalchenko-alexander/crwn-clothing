293838d08ef64f584b9d50122740691b02c1022f
aam <aam@anadeainc.com>
2020-08-30 19:02:18 +0300

    Reuse collection preview component on collection page

--- src/pages/CollectionPage/CollectionPage.component.jsx
+++ src/pages/CollectionPage/CollectionPage.component.jsx
@@ -3,15 +3,12 @@ import './CollectionPage.styles.scss';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { selectCollectionByName } from '../../redux/shop/shop-selectors';
-import CollectionItem from '../../components/CollectionPreview/CollectionItem/CollectionItem.component';
+import CollectionPreview from '../../components/CollectionPreview/CollectionPreview.component';
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return <div className="collection-page">
-    <h2 className="title">{ title }</h2>
-    <div className="items">
-      { items.map(item => <CollectionItem key={ item.id } item={ item } />) }
-    </div>
+    <CollectionPreview title={ title } items={ items } />
   </div>;
 };
 
