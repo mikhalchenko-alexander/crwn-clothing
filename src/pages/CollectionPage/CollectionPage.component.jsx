2f61ef38a279603a08cb4a17d1290d964f5d4436
aam <aam@anadeainc.com>
2020-10-15 18:24:39 +0300

    Cleanup collection page.

--- src/pages/CollectionPage/CollectionPage.component.jsx
+++ src/pages/CollectionPage/CollectionPage.component.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import './CollectionPage.styles.scss';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { selectCollectionByName } from '../../redux/shop/shop-selectors';
 import CollectionView from '../../components/CollectionView/CollectionView.component';
 
-const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
-  return <div className="collection-page">
-    <CollectionView title={ title } items={ items } />
-  </div>;
-};
+const CollectionPage = ({ collection }) => <CollectionView { ...collection } />;
 
 const mapStateToProps = (state, ownProps) => createStructuredSelector({
   collection: () => selectCollectionByName(ownProps.match.params.collectionId)(state)
