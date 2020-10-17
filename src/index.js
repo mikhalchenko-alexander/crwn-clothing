8704eb2fb8e78c220d82551d006f271de9f1f83a
Alexander <alexandr941989@gmail.com>
2020-10-17 22:23:11 +0300

    Migrate index component to styled components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { GlobalIndexStyles } from './index.styles';
 
 ReactDOM.render(
   <ReduxProvider store={ store }>
     <React.StrictMode>
       <BrowserRouter>
         <PersistGate persistor={ persistor }>
+          <GlobalIndexStyles />
           <App />
         </PersistGate>
       </BrowserRouter>
