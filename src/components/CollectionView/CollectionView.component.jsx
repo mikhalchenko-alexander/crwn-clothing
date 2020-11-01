b9af6c08e34ad532acb65d5621451690b606eb52
aam <aam@anadeainc.com>
2020-11-01 22:31:18 +0200

    Make collection title a link to the collection page

--- src/components/CollectionView/CollectionView.component.jsx
+++ src/components/CollectionView/CollectionView.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import CollectionItem from './CollectionItem/CollectionItem.component';
 import { CollectionViewContainer, TitleContainer, ViewContainer } from './CollectionView.styles';
+import { Link } from 'react-router-dom';
 
-const CollectionView = ({ title, items }) => (
+const CollectionView = ({ title, items, routeName }) => (
   <CollectionViewContainer>
-    <TitleContainer>{ title.toUpperCase() }</TitleContainer>
+    <TitleContainer>
+      <Link to={ `/shop/${ routeName }` }>{ title.toUpperCase() }</Link>
+    </TitleContainer>
     <ViewContainer>
       {
         items
