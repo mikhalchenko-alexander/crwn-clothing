93c8723770e55cb4a2dafe87c5dae5cfead92439
aam <aam@anadeainc.com>
2020-11-30 22:44:27 +0200

    Rewrite sign in component to functional component with hooks

--- src/components/SignIn/SignIn.component.jsx
+++ src/components/SignIn/SignIn.component.jsx
@@ -1,73 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormInput from '../_forms/FormInput/FormInput.component';
 import { ButtonContainer, ButtonStyles } from '../Button/Button.styles';
 import { ButtonsContainer, SignInContainer, TitleContainer } from './SignIn.styles';
 import { connect } from 'react-redux';
 import { createEmailSignInStartedAction, createGoogleSignInStartedAction } from '../../redux/user/user-actions';
 
-class SignIn extends React.Component {
+const SignIn = ({ signInWithGoogle, signInWithEmail }) => {
 
-  constructor(props) {
-    super(props);
+  const [userCredentials, setUserCredentials] = useState({
+    email: '',
+    password: ''
+  });
 
-    this.state = {
-      email: '',
-      password: ''
-    };
-  }
+  const { email, password } = userCredentials;
 
-  render() {
-    const { signInWithGoogle } = this.props;
-    return (
-      <SignInContainer>
-        <TitleContainer>I already have an account</TitleContainer>
-        <span>Sign in with your email and password</span>
-        <form onSubmit={ this.handleSubmit }>
-
-          <FormInput handleChange={ this.handleChange }
-                     label="Email"
-                     type="email"
-                     name="email"
-                     value={ this.state.email }
-                     required />
-
-          <FormInput handleChange={ this.handleChange }
-                     label="Password"
-                     type="password"
-                     name="password"
-                     value={ this.state.password }
-                     required />
-
-          <ButtonsContainer>
-            <ButtonContainer type="submit">Sign In</ButtonContainer>
-            <ButtonContainer type="button" buttonStyle={ ButtonStyles.GOOGLE_SIGN_IN } onClick={ signInWithGoogle }>SIGN
-              IN WITH
-              GOOGLE</ButtonContainer>
-          </ButtonsContainer>
-        </form>
-      </SignInContainer>
-    );
-  }
-
-  handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const { signInWithEmail } = this.props;
-    const { email, password } = this.state;
     signInWithEmail(email, password);
   };
 
-  clearForm = () => {
-    this.setState({
-      email: '',
-      password: ''
-    });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserCredentials({ ...userCredentials, [name]: value });
   };
 
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
-}
+  return (
+    <SignInContainer>
+      <TitleContainer>I already have an account</TitleContainer>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={ handleSubmit }>
+
+        <FormInput handleChange={ handleChange }
+                   label="Email"
+                   type="email"
+                   name="email"
+                   value={ email }
+                   required />
+
+        <FormInput handleChange={ handleChange }
+                   label="Password"
+                   type="password"
+                   name="password"
+                   value={ password }
+                   required />
+
+        <ButtonsContainer>
+          <ButtonContainer type="submit">Sign In</ButtonContainer>
+          <ButtonContainer type="button" buttonStyle={ ButtonStyles.GOOGLE_SIGN_IN } onClick={ signInWithGoogle }>SIGN
+            IN WITH
+            GOOGLE</ButtonContainer>
+        </ButtonsContainer>
+      </form>
+    </SignInContainer>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   signInWithGoogle: () => dispatch(createGoogleSignInStartedAction()),
