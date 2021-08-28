bd21d673f7a1e7809a8ce520eb070e78f302862a
Alexander <alexandr941989@gmail.com>
2021-08-28 20:52:45 +0300

    Migrate ShoppingCartDropdown to Redux hooks.

--- client/src/components/ShoppingCartIcon/ShoppingCartDropdown/ShoppingCartDropdown.component.jsx
+++ client/src/components/ShoppingCartIcon/ShoppingCartDropdown/ShoppingCartDropdown.component.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ShoppingCartItem from './ShoppingCartItem/ShoppingCartItem.component';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectShoppingCartItems } from '../../../redux/shopping-cart/shopping-cart-selectors';
-import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
 import { createToggleHiddenShoppingCartDropdownAction } from '../../../redux/shopping-cart/shopping-cart-dropdown/shopping-cart-dropdown-actions';
 import {
@@ -12,37 +11,29 @@ import {
   ShoppingCartDropdownContainer
 } from './ShoppingCartDropdown.styles';
 
-class ShoppingCartDropdown extends React.Component {
+const ShoppingCartDropdown = ({ history }) => {
 
-  handleGoToCheckout = () => {
-    const { history, toggleShoppingCartDropdownHidden } = this.props;
-    toggleShoppingCartDropdownHidden();
+  const dispatch = useDispatch();
+
+  const handleGoToCheckout = () => {
+    dispatch(createToggleHiddenShoppingCartDropdownAction());
     history.push('/checkout');
   };
 
-  render() {
-    const { shoppingCartItems } = this.props;
-    return (
-      <ShoppingCartDropdownContainer>
-        <CartItemsContainer>
-          {
-            shoppingCartItems.length ?
-              shoppingCartItems.map(item => <ShoppingCartItem key={ item.id } item={ item } />) :
-              <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
-          }
-        </CartItemsContainer>
-        <CheckoutButtonContainer onClick={ () => this.handleGoToCheckout() }>GO TO CHECKOUT</CheckoutButtonContainer>
-      </ShoppingCartDropdownContainer>
-    );
-  }
-}
+  const shoppingCartItems = useSelector(selectShoppingCartItems);
 
-const mapStateToProps = createStructuredSelector({
-  shoppingCartItems: selectShoppingCartItems
-});
+  return (
+    <ShoppingCartDropdownContainer>
+      <CartItemsContainer>
+        {
+          shoppingCartItems.length ?
+            shoppingCartItems.map(item => <ShoppingCartItem key={ item.id } item={ item } />) :
+            <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
+        }
+      </CartItemsContainer>
+      <CheckoutButtonContainer onClick={ () => handleGoToCheckout() }>GO TO CHECKOUT</CheckoutButtonContainer>
+    </ShoppingCartDropdownContainer>
+  );
+};
 
-const mapDispatchToProps = dispatch => ({
-  toggleShoppingCartDropdownHidden: () => dispatch(createToggleHiddenShoppingCartDropdownAction())
-});
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ShoppingCartDropdown));
+export default withRouter(ShoppingCartDropdown);
