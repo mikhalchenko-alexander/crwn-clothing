2d190ecbb12e814f0ad2fb51670833dc49e262bb
Alexander <alexandr941989@gmail.com>
2021-08-28 20:42:48 +0300

    Migrate CollectionPage to Redux hooks.

--- client/src/pages/CollectionPage/CollectionPage.component.jsx
+++ client/src/pages/CollectionPage/CollectionPage.component.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { createStructuredSelector } from 'reselect';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectCollectionByName } from '../../redux/shop/shop-selectors';
 import CollectionView from '../../components/CollectionView/CollectionView.component';
 
-const CollectionPage = ({ collection }) => <CollectionView { ...collection } />;
+const CollectionPage = ({ match }) => {
+  const collection = useSelector(selectCollectionByName(match.params.collectionId));
+  console.log(collection);
+  return <CollectionView { ...collection } />;
+};
 
-const mapStateToProps = (state, ownProps) => createStructuredSelector({
-  collection: () => selectCollectionByName(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
