8d70ae7a4f3a53ebfb97d968947f18ba8572cd0b
Alexander <alexandr941989@gmail.com>
2021-08-28 18:08:32 +0300

    Migrate Header to Redux hooks.

--- client/src/components/Header/Header.component.jsx
+++ client/src/components/Header/Header.component.jsx
@@ -1,44 +1,40 @@
 import React from 'react';
 import { ReactComponent as Logo } from './crown.svg';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ShoppingCartIcon from '../ShoppingCartIcon/ShoppingCartIcon.component';
 import ShoppingCartDropdown from '../ShoppingCartIcon/ShoppingCartDropdown/ShoppingCartDropdown.component';
 import { selectCurrentUser } from '../../redux/user/user-selectors';
 import { selectShoppingCartDropdownHidden } from '../../redux/shopping-cart/shopping-cart-dropdown/shopping-cart-dropdown-selectors';
-import { createStructuredSelector } from 'reselect';
 import { HeaderContainer, LogoContainer, OptionLink, OptionsContainer } from './Header.styles';
 import { createSignOutStartAction } from '../../redux/user/user-actions';
 
-const Header = ({ currentUser, shoppingCartDropdownHidden, signOut }) => (
-  <HeaderContainer>
-    <LogoContainer to="/">
-      <Logo className="logo" />
-    </LogoContainer>
-    <OptionsContainer>
-      <OptionLink to="/shop">SHOP</OptionLink>
-      <OptionLink to="/contact">CONTACT</OptionLink>
-      {
-        currentUser ?
-          <OptionLink as="div" onClick={ () => signOut() }>SIGN OUT</OptionLink> :
-          <OptionLink to="/sign-in">SIGN IN</OptionLink>
-      }
-      <ShoppingCartIcon />
-      {
-        shoppingCartDropdownHidden ?
-          null :
-          <ShoppingCartDropdown />
-      }
-    </OptionsContainer>
-  </HeaderContainer>
-);
+const Header = () => {
+  const currentUser = useSelector(selectCurrentUser);
+  const shoppingCartDropdownHidden = useSelector(selectShoppingCartDropdownHidden);
+  const dispatch = useDispatch();
 
-const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  shoppingCartDropdownHidden: selectShoppingCartDropdownHidden
-});
+  return (
+    <HeaderContainer>
+      <LogoContainer to="/">
+        <Logo className="logo" />
+      </LogoContainer>
+      <OptionsContainer>
+        <OptionLink to="/shop">SHOP</OptionLink>
+        <OptionLink to="/contact">CONTACT</OptionLink>
+        {
+          currentUser ?
+            <OptionLink as="div" onClick={ () => dispatch(createSignOutStartAction()) }>SIGN OUT</OptionLink> :
+            <OptionLink to="/sign-in">SIGN IN</OptionLink>
+        }
+        <ShoppingCartIcon />
+        {
+          shoppingCartDropdownHidden ?
+            null :
+            <ShoppingCartDropdown />
+        }
+      </OptionsContainer>
+    </HeaderContainer>
+  );
+};
 
-const mapDispatchToProps = dispatch => ({
-  signOut: () => dispatch(createSignOutStartAction())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default Header;
